f06c8557f2f461f6fe3fb214e0e12d50bb6cbc6a
Mykola Gavryliuk <gavrilyuk.nikolay@gmail.com>
2026-01-25 13:44:43 +0000

    added led pins clamping to decrease the consumption

--- firmwares/common/app_main.c
+++ firmwares/common/app_main.c
@@ -18,7 +18,6 @@
 #include "fsl_power.h"
 #include "fsl_wwdt.h"
 #include "pdum_gen.h"
-#include "pwrm.h"
 #include "zps_apl_af.h"
 #include "zps_apl_zdo.h"
 #include "zps_gen.h"
@@ -74,6 +73,7 @@ static void PreSleep(void) {
     if (device_config.bIsJoined) {
         vAppApiSaveMacSettings();
     }
+    LEDS_EnableClamp();
 #ifdef DEBUG_APP_ENABLED
     DbgConsole_Flush();
     DbgConsole_Deinit();
@@ -113,6 +113,7 @@ static void vAttemptToSleep(void) {
     }
 
     uint16_t u16Activities = mLPMFlag;
+
     if (POLL_IsRunning() && u16Activities > 0)
         u16Activities--;
 
@@ -135,7 +136,7 @@ static void vAttemptToSleep(void) {
                 (void)PWR_ChangeDeepSleepMode(PWR_E_SLEEP_OSCON_RAMON);
                 PWR_Init();
                 PWR_vForceRadioRetention(TRUE);
-                PWRM_teStatus u8Status = PWR_eScheduleActivity(&sWake, MAXIMUM_TIME_TO_SLEEP_SEC * 1000, WakeCallBack);
+                PWR_teStatus u8Status = PWR_eScheduleActivity(&sWake, MAXIMUM_TIME_TO_SLEEP_SEC * 1000, WakeCallBack);
                 bActivityScheduled = TRUE;
                 APP_MAIN_DBG("PWRM_eScheduleActivity status: %d\n", u8Status);
             } else {
