e54c03ccab69d3b6bc2c093d64e25cea912ff06a
Mykola Gavryliuk <gavrilyuk.nikolay@gmail.com>
2026-02-13 21:38:24 +0000

    updated pins for WXKG14LM; updated converters to mark attributes as writtable

--- firmwares/WXKG14LM/device_definitions.h
+++ firmwares/WXKG14LM/device_definitions.h
@@ -3,14 +3,14 @@
 
 // TODO: update pins
 /* Button defintions */
-#define BTN_CENTER_DIO (14)  // center button DIO
+#define BTN_CENTER_DIO (13)  // center button DIO
 #define BTN_CENTER_MASK (1 << BTN_CENTER_DIO)
 #define BTN_INTERRUPT_MASK BTN_CENTER_MASK
 #define BTN_RESET_MASK BTN_INTERRUPT_MASK
 /* Button defintions - end */
 
 /* Led definitions */
-#define LED_CENTER_DIO (18)
+#define LED_CENTER_DIO (3)
 #define LED_CENTER_MASK (1UL << LED_CENTER_DIO)
 #define RESET_LED_MASK LED_CENTER_MASK
 /* Led definitions - end */
